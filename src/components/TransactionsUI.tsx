bdfd229511029bb4b1e99544e46ae96f5ac52579
Michael Orts <michael.orts.86@gmail.com>
2025-03-25 22:18:47 +0100

    work on TransactionsUI

--- src/components/TransactionsUI.tsx
+++ src/components/TransactionsUI.tsx
@@ -1,18 +1,41 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TableUI from './TableUI';
+import { publicClientSepolia, publicClientHardhat } from '@/utils/client';
+import { useAccount, useWatchContractEvent } from 'wagmi';
+import { erc20Abi, formatEther } from 'viem';
 
 const TransactionsUI: React.FC = () => {
 
+    const [publicClient, setPublicClient] = useState<typeof publicClientSepolia>(publicClientSepolia);
+    const [data, setData] = useState<[string, string, string, string][]>([]);
+    const { address, chain } = useAccount();
+
+    useEffect(() => {
+            setPublicClient(chain?.name === "Hardhat" ? publicClientHardhat : publicClientSepolia);
+        }, [publicClient, chain]);
+
+    useWatchContractEvent({
+        abi: erc20Abi,
+        eventName: 'Transfer',
+        poll : true,
+        fromBlock: BigInt(0),
+        onLogs(logs) {
+            const newData: [string, string, string, string][] = logs.map(log => {
+                if ( log.args.from === address ) {
+                    return [log.address, "Send", log.args.to as `0x${string}`, formatEther(log.args.value as bigint)];
+                } else if ( log.args.to === address ) {
+                    return [log.address, "Receive", log.args.from as `0x${string}`, formatEther(log.args.value as bigint)];
+                }
+            }).filter((data): data is [string, string, string, string] => data !== undefined);
+            setData(newData);
+        }
+    });
+
     return (
         <div className="border-3 rounded-xl p-4">
-            <TableUI tableCaption="Transactions Done" headCaption={["Token", "Direction", "Address", "Amount"]} cellDatas={[
-                ["TKN", "Send", "0x012345678901234567890123", "10"],
-                ["TKN", "Receive", "0x987654321098765432109876", "25"],
-                ["OTH", "Receive", "0x012345678901234567890123", "8.05"],
-                ["LS", "Send", "0x987654321098765432109876", "120000"]
-            ]} />
+            <TableUI tableCaption="Transactions Done" headCaption={["Token", "Direction", "Address", "Amount"]} cellDatas={data} />
         </div>
     );
   }
